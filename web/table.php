6563400ab664b2e412c9b06f3ba0b985300cd313
Dominik Křivohlávek <domkrivohlavek@gmail.com>
2017-08-22 23:16:32 +0200

    fixed listing empty device list

--- web/table.php
+++ web/table.php
@@ -5,17 +5,24 @@
     $deviceRep = new DeviceRepository();
 
     $deviceList = $deviceRep->GetDevicesByLogin($_SESSION["login"]);
-    echo "<table>";
-        echo "<tr>";
-            echo "<th>Device name</th>";
-            echo "<th>Action</th>";
-        echo "</tr>";
-        foreach($deviceList as $device){
+    if($deviceList == NULL)
+    {
+        echo "No devices were found!";
+    }
+    else
+    {
+        echo "<table>";
             echo "<tr>";
-                echo "<td>".$device["Name"]."</td>";
-                echo "<td><button onclick=\"SeteShutdownState()\" id=\"shutdown\" name=\"" . $device["Device_id"] . "\">Shutdown</button></td>";
+                echo "<th>Device name</th>";
+                echo "<th>Action</th>";
             echo "</tr>";
-        }
-    echo "</table>";
-    echo '<script src="./js/shutdownButoonHandler.js"></script>'
+            foreach($deviceList as $device){
+                echo "<tr>";
+                    echo "<td>".$device["Name"]."</td>";
+                    echo "<td><button onclick=\"SeteShutdownState()\" id=\"shutdown\" name=\"" . $device["Device_id"] . "\">Shutdown</button></td>";
+                echo "</tr>";
+            }
+        echo "</table>";
+        echo '<script src="./js/shutdownButoonHandler.js"></script>';
+    }
 ?>
