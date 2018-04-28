14f8ef97f970b2eb24f40ad220537717672e57a5
Jan Stárek <starek4@centrum.cz>
2018-04-28 03:37:26 +0200

    Design changes + splash screen on Android

--- client/FlyPhone/FlyPhone.Android/Resources/Resource.Designer.cs
+++ client/FlyPhone/FlyPhone.Android/Resources/Resource.Designer.cs
@@ -3840,26 +3840,26 @@ namespace FlyPhone.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020134
-			public const int avd_hide_password_1 = 2130837812;
-			
 			// aapt resource value: 0x7f020135
-			public const int avd_hide_password_2 = 2130837813;
+			public const int avd_hide_password_1 = 2130837813;
 			
 			// aapt resource value: 0x7f020136
-			public const int avd_hide_password_3 = 2130837814;
+			public const int avd_hide_password_2 = 2130837814;
+			
+			// aapt resource value: 0x7f020137
+			public const int avd_hide_password_3 = 2130837815;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020137
-			public const int avd_show_password_1 = 2130837815;
-			
 			// aapt resource value: 0x7f020138
-			public const int avd_show_password_2 = 2130837816;
+			public const int avd_show_password_1 = 2130837816;
 			
 			// aapt resource value: 0x7f020139
-			public const int avd_show_password_3 = 2130837817;
+			public const int avd_show_password_2 = 2130837817;
+			
+			// aapt resource value: 0x7f02013a
+			public const int avd_show_password_3 = 2130837818;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -4506,11 +4506,11 @@ namespace FlyPhone.Droid
 			// aapt resource value: 0x7f02012b
 			public const int notification_icon_background = 2130837803;
 			
-			// aapt resource value: 0x7f020132
-			public const int notification_template_icon_bg = 2130837810;
-			
 			// aapt resource value: 0x7f020133
-			public const int notification_template_icon_low_bg = 2130837811;
+			public const int notification_template_icon_bg = 2130837811;
+			
+			// aapt resource value: 0x7f020134
+			public const int notification_template_icon_low_bg = 2130837812;
 			
 			// aapt resource value: 0x7f02012c
 			public const int notification_tile_bg = 2130837804;
@@ -4525,10 +4525,13 @@ namespace FlyPhone.Droid
 			public const int shutdown = 2130837807;
 			
 			// aapt resource value: 0x7f020130
-			public const int tooltip_frame_dark = 2130837808;
+			public const int splash = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int tooltip_frame_light = 2130837809;
+			public const int tooltip_frame_dark = 2130837809;
+			
+			// aapt resource value: 0x7f020132
+			public const int tooltip_frame_light = 2130837810;
 			
 			static Drawable()
 			{
@@ -6263,6 +6266,9 @@ namespace FlyPhone.Droid
 			
 			// aapt resource value: 0x7f0b0047
 			public const int RtlUnderlay_Widget_AppCompat_ActionButton_Overflow = 2131427399;
+			
+			// aapt resource value: 0x7f0b0192
+			public const int Splashscreen = 2131427730;
 			
 			// aapt resource value: 0x7f0b00d6
 			public const int TextAppearance_AppCompat = 2131427542;
