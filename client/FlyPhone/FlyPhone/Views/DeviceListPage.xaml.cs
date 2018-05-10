efcf887a909417fab06ef81794f4f6d5cbbface1
Jan Stárek <starek4@centrum.cz>
2018-05-10 20:42:47 +0200

    Phone: Fixed changing favourite state in iOS and UWP app.

--- client/FlyPhone/FlyPhone/Views/DeviceListPage.xaml.cs
+++ client/FlyPhone/FlyPhone/Views/DeviceListPage.xaml.cs
@@ -14,18 +14,31 @@ namespace FlyPhone.Views
 
 	    private async void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
 	    {
-	        if (sender is Button button)
-	        {
+	        Image image = null;
+
+	        if (sender is Button button)    // UWP - button is under the image in GRID
+            {
 	            Grid grid = (Grid)button.Parent;
 	            foreach (var gridChild in grid.Children)
 	            {
-	                if (gridChild is Image image)
+	                if (gridChild is Image imageChild)
 	                {
+	                    image = imageChild;
 	                    await image.FadeTo(0.2, 100);
 	                    await image.FadeTo(1, 0);
                     }
 	            }
 	        }
+            else if (sender is Image imageSender)   // Android, iOS - image has tap event
+            {
+	            image = imageSender;
+	        }
+
+	        if (image != null)
+	        {
+	            await image.FadeTo(0.2, 100);
+	            await image.FadeTo(1, 0);
+            }
 	    }
     }
 }
